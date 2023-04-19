8114d459391632bb26baf1762dcfe5f5af02b90e
Parthiv0311 <122387846+Parthiv0311@users.noreply.github.com>
2023-04-18 21:03:59 -0400

    Add files via upload

---
+++ Final Exam/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $(function () {
+        $("#head").load("./idk/meta.html");
+        $("#header").load("./idk/header.html");
+        $("#footer").load("./idk/footer.html");
+    });
+});
